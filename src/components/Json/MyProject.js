6f5ab779a52fdb094dd9d1ad3ae10c709b6c4ac0
Eng/Ibrahim AL-shabrawishy <ibrahimelshabrawishy123321@gmail.com>
2024-12-05 13:19:49 +0200

    first commit

---
+++ src/components/Json/MyProject.js
@@ -0,0 +1,117 @@
+/** ============================================
+ table of contents
+================================================
+ 
+ 1. HTML &  CSS Project
+     2. JavaScript Project
+  3. React Js Project
+ 4. Vue.Js Project
+
+ 
+*/
+
+const myProjects = [
+  /* *================================================
+     1. HTML &  CSS Project
+  *=================================================== */
+
+  {
+    id: 1,
+    projectTitle: "Fokir Personal",
+    category: "css",
+    imagePath: "/assets/images/09ec474019a6749f650d27d72a8af135_thumb.jpg",
+    text: "HTML,CSS,Bootstrap",
+    liveLink: "https://ibrahimal-shabrawishy.github.io/Fokir-personal/",
+    githubLink: "https://github.com/IbrahimAl-shabrawishy/Fokir-personal.git",
+    tags: ["CSS", "HTML", "Design"],
+  },
+
+  {
+    id: 2,
+    projectTitle: "Daniels",
+    category: "css",
+    imagePath: "/assets/images/09ec474019a6749f650d27d72a8af135_thumb.jpg",
+    text: "HTML,CSS,Bootstrap",
+    liveLink: "https://ibrahimal-shabrawishy.github.io/Daniels-/",
+    githubLink: "https://github.com/IbrahimAl-shabrawishy/Daniels-.git",
+    tags: ["CSS", "HTML", "Design"],
+  },
+
+  /* *================================================
+     2. JavaScript Project
+  *=================================================== */
+
+  {
+    id: 3,
+    projectTitle: "CRUD Operation",
+    category: "javascript",
+    imagePath: "/assets/images/09ec474019a6749f650d27d72a8af135_thumb.jpg",
+    text: "JavaScript Projects",
+    liveLink: "https://ibrahimal-shabrawishy.github.io/Cruds-Operation/",
+    githubLink: "https://github.com/IbrahimAl-shabrawishy/Cruds-Operation",
+    tags: ["JavaScript", "CRUD", "Web Development"],
+  },
+  {
+    id: 4,
+    projectTitle: "ToDo List Using API",
+    category: "javascript",
+    imagePath: "/assets/images/09ec474019a6749f650d27d72a8af135_thumb.jpg",
+    text: "JavaScript Projects",
+    liveLink: "https://ibrahimal-shabrawishy.github.io/To-Do-List/",
+    githubLink: "https://github.com/IbrahimAl-shabrawishy/To-Do-List",
+    tags: ["JavaScript", "ToDo List", "Web Development"],
+  },
+  {
+    id: 5,
+    projectTitle: "Registration Form",
+    category: "javascript",
+    imagePath: "/assets/images/09ec474019a6749f650d27d72a8af135_thumb.jpg",
+    text: "JavaScript Projects",
+    liveLink: "https://ibrahimal-shabrawishy.github.io/Regestration-Page/",
+    githubLink: "https://github.com/IbrahimAl-shabrawishy/Regestration-Page",
+    tags: ["JavaScript", "Registration Form", "Web Development"],
+  },
+
+  /* *================================================
+     3. React Js Project
+  *=================================================== */
+
+  {
+    id: 6,
+    projectTitle: "My Portfolio",
+    category: "react js",
+    imagePath: "/assets/images/09ec474019a6749f650d27d72a8af135_thumb.jpg",
+    text: "An interactive React.js project",
+    liveLink: "https://react-portfolio-9e280.web.app/",
+    githubLink: "https://github.com/IbrahimAl-shabrawishy/My-Portfolio.git",
+    tags: ["React", "JavaScript", "Responsive Design"],
+  },
+
+  {
+    id: 7,
+    projectTitle: "Ecommerce",
+    category: "react js",
+    imagePath: "/assets/images/09ec474019a6749f650d27d72a8af135_thumb.jpg",
+    text: "An interactive React.js project",
+    liveLink: "https://ecommerce-c75bb.web.app/",
+    githubLink: "https://github.com/IbrahimAl-shabrawishy/Ecommerce.git",
+    tags: ["React", "JavaScript", "Responsive Design"],
+  },
+
+  /* *================================================
+     4. Vue.Js Project
+  *=================================================== */
+
+  {
+    id: 7,
+    projectTitle: "Vue.js Project",
+    category: "vue",
+    imagePath: "/assets/images/09ec474019a6749f650d27d72a8af135_thumb.jpg",
+    text: "A Vue.js Project",
+    liveLink: "https://example.com/vue-project",
+    githubLink: "https://github.com/username/vue-project",
+    tags: ["Vue.js", "JavaScript", "SPA"],
+  },
+];
+
+export default myProjects;
